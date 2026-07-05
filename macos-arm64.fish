950c94036bdd4822da59837c58a682b5a031b4c5
Akatsumekusa <Akatsumekusa@protonmail.com>
2026-07-05 12:27:21 +0000

    Setup

--- macos-arm64.fish
+++ macos-arm64.fish
@@ -1,4 +1,4 @@
 #!/usr/bin/env fish
 
-argparse "static" "shared" "base-arch-only" "dovi-hdr10plus" "ffms2" "cmakeflags=" "cflags-profiling=" "cxxflags-profiling=" "ldflags-profiling=" "cflags-final=" "cxxflags-final=" "ldflags-final=" -- $argv
+argparse "static=" "shared=" "base-arch-only=" "dovi-hdr10plus=" "ffms2=" "cmakeflags=" "cflags-profiling=" "cxxflags-profiling=" "ldflags-profiling=" "cflags-final=" "cxxflags-final=" "ldflags-final=" -- $argv
 or return 1
