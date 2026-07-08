b7062b36823222f06997fb6ce5ea56fe0c5f6370
Akatsumekusa <Akatsumekusa@protonmail.com>
2026-07-08 16:27:27 +0000

    Finalise

--- build.fish
+++ build.fish
@@ -254,6 +254,10 @@ function pgo_build
         end
 
         for ffms2 in "base" (test $flag_ffms2 != "false" ; and echo "ffms2")
+            if begin test $flag_ffms2 != "false" ; test $os = "Windows" ; and test $ffms2 = "base" ; end
+                continue
+            end
+
             rm -rf svt_build Build
             or return $status
     
